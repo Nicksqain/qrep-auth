2bc5bf681bbeec73c39de0f08a5815d95699e9ba
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-10-20 03:30:57 +0500

    Update sentOTP api service (resend)
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- src/api/services/otpAuthServices.ts
+++ src/api/services/otpAuthServices.ts
@@ -1,9 +1,14 @@
 import { siteAPI } from "..";
 
-export const sendOTP = async (contact: string, messenger?: string) => {
+export const sendOTP = async (
+  contact: string,
+  resend?: boolean,
+  messenger?: string
+) => {
   const queryParams = new URLSearchParams();
-  queryParams.append('contact', contact);
-  messenger && queryParams.append('messenger', messenger);
+  queryParams.append("contact", contact);
+  messenger && queryParams.append("messenger", messenger);
+  resend && queryParams.append("resend", resend.valueOf().toString());
   const url = `/send-otp?${queryParams.toString()}`;
   const response = await siteAPI.get(url);
   return response.data;
