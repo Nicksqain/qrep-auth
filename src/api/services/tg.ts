12aa8f7ada4b776e114c29ffdcbc101e591844ad
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-12-15 13:12:08 +0500

    Remove Telegram messaging service implementation
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- src/api/services/tg.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const token = import.meta.env.VITE_APP_TELEGRAM_BOT_TOKEN;
-
-export const sendTelegramMessage = (chatId: string, message: string) =>
-  axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
-    chat_id: '1199080883',
-    text: message,
-    parse_mode: "Markdown",
-  });
