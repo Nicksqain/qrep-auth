c8445b2de9b36d54a05420e74ac220f59a584215
Alexei Karikov <karikov.alist.ru@gmail.com>
2025-02-10 21:37:07 +0500

    Refactor input components for improved accessibility and consistency
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,5 +1,5 @@
 import { FC, memo, useCallback, useEffect, useRef, useState } from 'react';
-import { Box, Button, Heading, Image, HStack, Input, PinInput, PinInputField, Stack, Text, VStack, Card, ScaleFade, Tooltip, Flex, FormControl, useToast, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Button, Heading, Image, HStack, Input, Stack, Text, VStack, Flex, Field, useBreakpointValue, PinInput, Group } from '@chakra-ui/react';
 import { motion, useAnimate } from 'framer-motion';
 import Logo from '../../components/Logo';
 import { useMutation, useQuery } from '@tanstack/react-query';
@@ -19,6 +19,9 @@ import LanguageSwitcher from '../../shared/components/LanguageSwitcher';
 import { useTranslation } from 'react-i18next';
 import { setLanguage } from '../../slices/language.slice';
 import { Language } from '../../constants/languages';
+import { toaster } from '../../components/ui/toaster';
+import { Tooltip } from '../../components/ui/tooltip';
+import { FloatingInput } from '../../components/ui/floating-input';
 
 interface AuthProps { }
 
@@ -28,23 +31,23 @@ const MotionBox = motion(Box);
 const ResendButton = memo(({ countdown, isButtonDisabled, handleResendCode }: { countdown: number, isButtonDisabled: boolean, handleResendCode: () => void }) => {
   const { t } = useTranslation()
   return (
-    <Button colorScheme="blue" mt={4} onClick={handleResendCode} isDisabled={isButtonDisabled}>
+    <Button variant={"outline"} w={"100%"} mt={4} onClick={handleResendCode} disabled={isButtonDisabled}>
       {`${t('resend_code')} ${isButtonDisabled ? `(${countdown})` : ''}`}
     </Button>
   );
 });
 
 const Auth: FC<AuthProps> = () => {
-  const toast = useToast()
   const { t } = useTranslation()
   // Введен ли номер телефона или email
   const [phoneSubmitted, setPhoneSubmitted] = useState(false);
   const [phoneNumber, setPhoneNumber] = useState('');
   const [isValidPhone, setIsValidPhone] = useState(false);
   const [isInvalidOTP, setIsInvalidOTP] = useState(false);
-  const [otp, setOtp] = useState('');
+  const [otp, setOtp] = useState<string[]>(Array(6).fill(""));
   const [isEmailLogin, setIsEmailLogin] = useState(false); // Состояние для переключения между email и телефоном
   const [email, setEmail] = useState(''); // Email
+  const [isTouched, setIsTouched] = useState(false);
   const [emailError, setEmailError] = useState(''); // Email validation error
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
   const [countdown, setCountdown] = useState(0);
@@ -66,7 +69,7 @@ const Auth: FC<AuthProps> = () => {
   }, [isCIS]);
 
   const handlePhoneSubmit = async () => {
-    if (phoneNumber) {
+    if (phoneNumber && isValidPhone) {
       // setPhoneSubmitted(true);
       mutation.mutate(phoneNumber);
     }
@@ -95,15 +98,14 @@ const Auth: FC<AuthProps> = () => {
     onSuccess: () => {
       setPhoneSubmitted(true);
       setCountdown(60);
-      setOtp('');
+      setOtp(["", "", "", "", "", ""]);
     },
     onError: (error) => {
-      toast({
+      toaster.create({
         title: 'Ошибка',
         description: `Не удалось отправить код. Проверьте данные.`,
-        status: 'error',
+        type: 'error',
         duration: 3000,
-        isClosable: true,
       });
       console.error('Ошибка при отправке сообщения:', error);
     },
@@ -116,15 +118,14 @@ const Auth: FC<AuthProps> = () => {
       return res;
     },
     onSuccess: () => {
-      setOtp('');
+      setOtp(["", "", "", "", "", ""]);
       setCountdown(60);
-      toast({
+      toaster.create({
         title: 'Отправлено',
-        position: 'top',
+        placement: 'top',
         description: "Код верификации отправлен повторно",
-        status: 'success',
+        type: 'success',
         duration: 3000,
-        isClosable: true,
       });
     },
     onError: (error) => {
@@ -164,12 +165,11 @@ const Auth: FC<AuthProps> = () => {
     navigate({ search: params.toString() }, { replace: true });
 
     if (referrer?.endsWith('/order')) {
-      toast({
+      toaster.create({
         title: t('authorization'),
         description: t('track_orders'),
-        status: 'info',
-        duration: null,
-        isClosable: true,
+        type: 'info',
+        duration: undefined,
       });
     }
   }, [location.search, dispatch, navigate]);
@@ -180,8 +180,8 @@ const Auth: FC<AuthProps> = () => {
 
   const { status, data, error } = useQuery({
     queryKey: ['verifyOTP', phoneNumber, otp],
-    queryFn: () => isEmailLogin ? verifyOTP(siteURL, email, otp) : verifyOTP(siteURL, phoneNumber, otp),
-    enabled: phoneSubmitted && otp.length === 6,
+    queryFn: () => isEmailLogin ? verifyOTP(siteURL, email, otp?.join('') || '') : verifyOTP(siteURL, phoneNumber, otp?.join('') || ''),
+    enabled: phoneSubmitted && otp.join('')?.length === 6,
     refetchOnWindowFocus: false,
     refetchOnMount: false,
     staleTime: 2000,
@@ -191,44 +191,41 @@ const Auth: FC<AuthProps> = () => {
   // При успешной верификации OTP
   useEffect(() => {
     if (status === 'success') {
-      if (otp.length === 6) {
+      if (otp.join('')?.length === 6) {
         if (data.is_valid) {
-        toast({
+          toaster.create({
           title: 'Успех',
           description: "Добро пожаловать!",
-          status: 'success',
-          duration: 1000,
-          isClosable: true,
+            type: 'success',
+            duration: 1000,
         });
         if (data.auth_redirect) {
           const baseUrl = new URL(data.auth_redirect);
           const params = new URLSearchParams(baseUrl.search);
           params.append('referrer', referrer ?? '');
           baseUrl.search = params.toString();
-          window.location.replace(baseUrl.toString());
+          window.parent.postMessage({ type: 'auth_success', redirectUrl: baseUrl.toString() }, '*');
           }
         }
         else {
           setIsInvalidOTP(true);
           setTimeout(() => setIsInvalidOTP(false), 200);
-          setOtp('');
+          setOtp(["", "", "", "", "", ""]);
           pinInputRef.current?.focus();
-          toast({
+          toaster.create({
             title: 'Ошибка',
             description: `Неверный код верификации`,
-            status: 'error',
+            type: 'error',
             duration: 1000,
-            isClosable: true,
           });
         }
       }
     } else if (status === 'error') {
-      toast({
+      toaster.create({
         title: 'Ошибка',
         description: `Ошибка при проверке OTP`,
-        status: 'error',
+        type: 'error',
         duration: 1000,
-        isClosable: true,
       })
     }
   }, [status, data, error]);
@@ -283,36 +280,39 @@ const Auth: FC<AuthProps> = () => {
   }, [phoneSubmitted, isEmailLogin]);
 
   return (
-    <ScaleFade initialScale={0.9} in>
-      <Card className='auth-card' variant={"outline"} borderRadius={10}>
+    <Box>
+      <Box className='auth-card' borderRadius={10}>
         <VStack className='auth-container' align="start" textAlign={"start"} gap={10}>
-          <HStack justifyContent={"space-between"} w={"100%"}><Logo /><LanguageSwitcher /></HStack>
-          <HStack className='auth-content-box' justifyContent={"space-between"} height={330} width="100%" spacing={8}>
-            <VStack className='auth-form' align={"start"} w={350}>
-              <Heading size={"lg"} mb={6}>
+          <HStack className='auth-content-box' justifyContent={"space-between"} align={"start"} height={330} width="100%" gap={16}>
+            <VStack className='auth-form' w={"100%"}>
+              <Heading size={"lg"} textTransform={"uppercase"} textAlign={"start"} w={"100%"}>
                 {phoneSubmitted ? (mutation.data?.isRegistered ? t('login') : t('registration')) : t('login_registration')}
               </Heading>
 
               {!phoneSubmitted && (
-                <Stack spacing={10}>
-                  <Flex alignItems="start">
-                    <Text>{isEmailLogin ? t('enter_email') : t('enter_phone_number')}</Text>
-                    <Tooltip label={t('phone_number_info')}>
+                <Stack gap={10} w={"100%"}>
+                  <Flex justify={"space-between"} gap={2}>
+                    <Text w={"350px"}>{isEmailLogin ? t('enter_email') : t('enter_phone_number')}</Text>
+                    <Tooltip content={t('phone_number_info')}>
                       <Flex><InfoIcon width={25} height={25} /></Flex>
                     </Tooltip>
                   </Flex>
-                  <Stack spacing={4}>
+                  <Stack gap={4}>
                     {isEmailLogin ? (
-                      <FormControl isInvalid={!validateEmail(email)}>
-                        <Input
+                      <Field.Root invalid={isTouched && !validateEmail(email)}>
+                        <FloatingInput
+                          label='Email'
+                          variant={'flushed'}
+                          height={"48px"}
                           size="lg"
                           ref={emailInputRef}
-                          placeholder="Email"
                           value={email}
                           onChange={(e) => {
                             setEmail(e.target.value);
                           }}
+                          onFocus={() => setIsTouched(false)}
                           type="email"
+                          onBlur={() => setIsTouched(true)}
                           onKeyDown={(e) => {
                             if (e.key === 'Enter' && email) {
                               handleEmailSubmit();
@@ -320,7 +320,7 @@ const Auth: FC<AuthProps> = () => {
                           }}
                         />
                         {/* {emailError && <FormErrorMessage>{emailError}</FormErrorMessage>} */}
-                      </FormControl>
+                      </Field.Root>
                     ) : (
                       <>
                         <PhoneInput
@@ -333,6 +333,8 @@ const Auth: FC<AuthProps> = () => {
                           placeholder="Номер телефона"
                           type="tel"
                           autoComplete='phone'
+                            onFocus={() => setIsTouched(false)}
+                            onBlur={() => setIsTouched(true)}
                           onKeyDown={(e) => {
                             if (e.key === 'Enter' && phoneNumber) {
                               handlePhoneSubmit();
@@ -341,39 +343,52 @@ const Auth: FC<AuthProps> = () => {
                           />
                       </>
                     )}
-                    <Button isLoading={isLoading} size="lg" colorScheme="blue" onClick={isEmailLogin ? handleEmailSubmit : handlePhoneSubmit} isDisabled={isEmailLogin ? !email : !isValidPhone}>
+                    <Button variant={"outline"} w={"100%"} loading={isLoading} size="lg" colorScheme="blue" onClick={isEmailLogin ? handleEmailSubmit : handlePhoneSubmit} disabled={isEmailLogin ? !validateEmail(email) : !isValidPhone}>
                       {t('send_code')}
                     </Button>
-                    <Button variant="link" onClick={() => setIsEmailLogin(!isEmailLogin)}>
+                    <Button variant="plain" w={"100%"} onClick={() => setIsEmailLogin(!isEmailLogin)}>
                       {isEmailLogin ? t('login_with_phone') : t('login_with_email')}
                     </Button>
                   </Stack>
                 </Stack>
               )}
-
+              {/* <PinInput.Root placeholder='-' size={"lg"} otp value={otp} variant={"outline"} colorScheme='red' onValueChange={(e) => {
+                console.debug('value', e);
+                setIsInvalidOTP(false);
+                setOtp(e.value);
+              }} invalid={isInvalidOTP}>
+                <PinInput.HiddenInput />
+                <PinInput.Control>
+                  {Array.from({ length: 6 }).map((_, index) => (
+                    <PinInput.Input key={index} index={index} value={otp[index]} />
+                  ))}
+                </PinInput.Control>
+              </PinInput.Root> */}
               {phoneSubmitted && (
                 <VStack align={"start"}>
                   <Box mb={4}>
                     <Text>{isEmailLogin ? t('enter_code_sent_email') : t('enter_code_sent', { source: mutation.data?.final_source })}</Text>
-                    {/* <Text>{isEmailLogin ? email : '+7 702 596 2345'}</Text> */}
                   </Box>
                   <HStack>
-                    <PinInput size={"lg"} value={otp} onChange={(value) => {
+                    <PinInput.Root size={{ base: "md", md: "2xl" }} placeholder='—' otp value={otp} variant={"flushed"} onValueChange={(e) => {
+                      console.debug('value', e);
                       setIsInvalidOTP(false);
-                      setOtp(value);
-                    }} isInvalid={isInvalidOTP}>
-                      <PinInputField ref={pinInputRef} />
-                      <PinInputField />
-                      <PinInputField />
-                      <PinInputField />
-                      <PinInputField />
-                      <PinInputField />
-                    </PinInput>
+                      setOtp(e.value);
+                    }} invalid={isInvalidOTP}>
+                      <PinInput.HiddenInput />
+                      <PinInput.Control>
+                        <Group gap={2}>
+                          {Array.from({ length: 6 }).map((_, index) => (
+                            <PinInput.Input w={"100%"} key={index} index={index} ref={index === 0 ? pinInputRef : undefined} />
+                          ))}
+                        </Group>
+                      </PinInput.Control>
+                    </PinInput.Root>
                   </HStack>
 
                   <ResendButton countdown={countdown} isButtonDisabled={isButtonDisabled} handleResendCode={handleResendCode} />
 
-                  <Button variant="link" mt={4} onClick={handleResetPhone}>
+                  <Button variant="plain" w={"100%"} mt={4} onClick={handleResetPhone}>
                     {isEmailLogin ? t('enter_another_email') : t('enter_another_phone')}
                   </Button>
                 </VStack>
@@ -381,114 +396,18 @@ const Auth: FC<AuthProps> = () => {
             </VStack>
             {isMobile ? <QRLinks /> :
               <>
-                <MotionBox
-                  initial={{ opacity: 0, width: 0 }}
-                  animate={{ opacity: 1, width: !phoneSubmitted ? 330 : 0 }}
-                  transition={{ duration: 0.5, ease: [0.4, 0, 0.2, 1] }}
-                  overflow="hidden"
-                  className="motion-box"
+                <Box
+                  w={"100%"}
                 >
                   <QRLinks />
-                </MotionBox>
-                <MotionBox
-                  className={"hide-on-small"}
-                  key={`image-${phoneSubmitted}`}
-                  initial={{ opacity: 0, width: 0 }}
-                  exit={{ opacity: 0, width: 330 }}
-                  animate={{ opacity: phoneSubmitted ? 1 : 0, width: phoneSubmitted ? 330 : 0 }}
-                  transition={{ duration: 0.5, ease: [0.4, 0, 0.2, 1] }}
-                  overflow="hidden"
-                  // borderLeft="1px solid #e2e8f0"
-                  onAnimationComplete={() => {
-                    if (phoneSubmitted) {
-                      myAnimation();
-                    }
-                  }}
-                >
-                  <Box style={{ position: "relative", backgroundColor: "#d3dfed", borderRadius: 10 }} height={330}>
-                    <Image
-                      ref={phoneImageRef}
-                      zIndex={1}
-                      style={{ position: "absolute", top: 0, right: 0, transform: 'translate(330px, -120px)' }}
-                      src={PhoneSVG}
-                      alt="Phone verification"
-                      display={phoneSubmitted ? 'block' : 'none'}
-                    />
-                    <Image
-                      ref={notificationImageRef}
-                      zIndex={2}
-                      transformOrigin={"bottom"}
-                      style={{ position: "absolute", top: 0, right: 0, transform: 'translate(330px, -120px)' }}
-                      src={NotificationSVG}
-                      alt="Phone verification"
-                      display={phoneSubmitted ? 'block' : 'none'}
-                    />
-                  </Box>
-                </MotionBox>
+                </Box>
               </>
             }
           </HStack>
         </VStack>
-      </Card>
-    </ScaleFade>
+      </Box >
+    </Box >
   );
 };
 
 export default memo(Auth);
-
-
-
-// ! 1 version
-// {!phoneSubmitted && (
-//   <MotionBox
-//   initial={{ opacity: 0 }}
-//   animate={{ opacity: 1 }}
-//   exit={{ opacity: 0 }}
-//   transition={{ duration: 0.5, ease: [0.4, 0, 0.2, 1] }}
-//   overflow="hidden"
-//   >
-//   <QRLinks />
-//   </MotionBox>
-// )}
-// <MotionBox
-//   initial={{ opacity: 0, width: 0 }}
-//   animate={{ opacity: phoneSubmitted ? 1 : 0, width: phoneSubmitted ? 300 : 0 }}
-//   transition={{ duration: 0.5, ease: [0.4, 0, 0.2, 1] }}
-//   overflow="hidden"
-//   borderLeft="1px solid #e2e8f0"
-//   onAnimationComplete={myAnimation}
-// >
-//   <Image
-//   ref={imageRef}
-//   height={"100%"}
-//   src="/phone.png"
-//   alt="Phone verification"
-//   display={phoneSubmitted ? 'block' : 'none'}
-//   />
-// </MotionBox>
-
-// ! 2 version
-{/* <MotionBox
-              initial={{ opacity: 0, width: 0 }}
-              animate={{ opacity: 1, width: !phoneSubmitted ? 300 : 0 }}
-              transition={{ duration: 0.5, ease: [0.4, 0, 0.2, 1] }}
-              overflow="hidden"
-              >
-              <QRLinks />
-              </MotionBox>
-            <MotionBox
-              initial={{ opacity: 0, width: 0 }}
-              animate={{ opacity: phoneSubmitted ? 1 : 0, width: phoneSubmitted ? 300 : 0 }}
-              transition={{ duration: 0.5, ease: [0.4, 0, 0.2, 1] }}
-              overflow="hidden"
-              borderLeft="1px solid #e2e8f0"
-              onAnimationComplete={myAnimation}
-            >
-              <Image
-              ref={imageRef}
-              height={"100%"}
-              src="/phone.png"
-              alt="Phone verification"
-              display={phoneSubmitted ? 'block' : 'none'}
-              />
-            </MotionBox> */}
