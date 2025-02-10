c1c7c2510eef0f87b4a884995434752eda063647
Alexei Karikov <karikov.alist.ru@gmail.com>
2025-02-10 21:32:01 +0500

    Refactor QRLinks component for improved layout and styling consistency
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- src/pages/Auth/components/QRLinks/index.tsx
+++ src/pages/Auth/components/QRLinks/index.tsx
@@ -14,33 +14,33 @@ const QRLinks: FC<QRLinksProps> = () => {
   const { t } = useTranslation();
 
   return (
-    <VStack align={"start"} w={330} overflow={"hidden"}>
-      <Heading size={"md"}>{t('qr_in_your_phone')}</Heading>
+    <VStack align={"start"} overflow={"hidden"} textAlign={"center"}>
+      <Heading size={"md"} w={"100%"}>{t('qr_in_your_phone')}</Heading>
       <Text>{t('use_chatbots')}</Text>
       {
         !isMobile ?
-          <Box>
-            <HStack>
+          <HStack justifyContent={"center"} w={"100%"}>
+            <VStack>
               <Image height={100} src={tgbotqr} alt="" />
               <Text>
                 QR в{' '}
-                <Link color='telegram.500' textDecoration={"underline dotted"} href='https://t.me/qrep1465bot' target='_blank'>
+                <Link color='blue.400' textDecoration={"underline dotted"} href='https://t.me/qrep1465bot' target='_blank'>
                   Telegram
                 </Link>
               </Text>
-            </HStack>
-            <HStack>
+            </VStack>
+            <VStack>
               <Image height={100} src={wabotqr} alt="" />
               <Text>
                 QR в{' '}
-                <Link color='whatsapp.500' textDecoration={"underline dotted"} href='https://wa.me/77015721064' target='_blank'>
+                <Link color='green.500' textDecoration={"underline dotted"} href='https://wa.me/77015721064' target='_blank'>
                   WhatsApp
                 </Link>
               </Text>
-            </HStack>
-          </Box>
+            </VStack>
+          </HStack>
           :
-          <HStack gap={5}>
+          <HStack gap={5} justifyContent={"center"} w={"100%"}>
             <HStack gap={4}>
               <Image height={"3rem"} src={tgIcon} alt="" />
               <Link color='telegram.500' textDecoration={"underline dotted"} href='tg://resolve?domain=qrep1465bot' target='_blank'>
