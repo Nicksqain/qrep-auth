97c5b68df4e78c2701c97751186179ab406a7aae
Alexei Karikov <karikov.alist.ru@gmail.com>
2025-02-12 02:06:59 +0500

    Add request interceptor to siteAPI for language header management
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,3 +3,14 @@ import axios from "axios";
 export const siteAPI = axios.create({
   baseURL: `${import.meta.env.VITE_APP_SITE_URL}`,
 });
+
+siteAPI.interceptors.request.use(
+  (config) => {
+    config.headers["Accept-Language"] =
+      localStorage.getItem("language") || "kz";
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
